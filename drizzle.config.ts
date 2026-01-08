b4c9f5ccc3fd80192e95c269525eef8888c28880
Ahmad Elsagheer <ahmad.muhammad.elsagheer@gmail.com>
2026-01-08 14:56:36 +0200

    improve

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config"
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-    out: "./migrations",
-    schema: "./database/schema.ts",
+    out: "./src/migrations",
+    schema: "./src/database/schema.ts",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!
